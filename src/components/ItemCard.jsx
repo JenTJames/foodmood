0e938d6ffb9a78efca5e882ba5360af4fa022338
JenTJames <jenthomasjames@gmail.com>
2023-07-02 12:19:02 +0530

    Feat: Add food images

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import FastfoodRoundedIcon from "@mui/icons-material/FastfoodRounded";
 import AppButton from "./AppButton";
 
-const ItemCard = () => {
+const ItemCard = ({ image = null, title, desc }) => {
   return (
-    <div className="flex flex-col gap-2 p-2 rounded-lg border">
-      <div className="h-44 w-44 rounded-md bg-slate-300 md:min-w-full lg:h-72"></div>
-      <h4 className="font-semibold">Creamy Tomato Basil Soup</h4>
-      <p>Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs</p>
+    <div className="flex flex-col gap-5 p-2 rounded-lg border w-64 flex-shrink-0 lg:w-96 lg:flex-shrink justify-between">
+      {!image && (
+        <div className="h-44 w-44 flex text-8xl text-slate-200 justify-center items-center rounded-md bg-slate-300 md:min-w-full lg:h-72">
+          <FastfoodRoundedIcon fontSize="inherit" color="inherit" />
+        </div>
+      )}
+      {image && (
+        <img
+          className="h-44 w-44 rounded-md min-w-full lg:h-72 object-cover"
+          src={image}
+          alt=""
+        />
+      )}
+      <h4 className="font-semibold text-slate-800">{title}</h4>
+      <p className="text-slate-600 text-justify">{desc}</p>
       <AppButton bold variant="outlined">
         View More
       </AppButton>
