095e5a1cad38a2786c0c9eeef417ce8e2060200b
JenTJames <jenthomasjames@gmail.com>
2023-07-09 14:33:04 +0530

    Feat: Modify http hook to destructure config properties * Add test properties to config

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -5,30 +5,32 @@ const useHttp = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fireRequest = useCallback(async (config) => {
-    setIsLoading(true);
-    const apiKey = process.env.REACT_APP_API_KEY;
-    try {
-      const response = await axios({
-        method: config.method ? config.method : "GET",
-        url: `${process.env.REACT_APP_API_URL + config.endpoint}`,
-        // url: `${process.env.REACT_APP_TEST_API_URL}`,
-        data: config.payload ? config.payload : null,
-        headers: {
-          "x-api-key": apiKey,
-        },
-      });
-      return response;
-    } catch (error) {
-      setError({
-        isError: true,
-        serverError: error,
-        status: error.response?.status,
-      });
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
+  const fireRequest = useCallback(
+    async ({ method, isTest = false, testUrl, endpoint, payload }) => {
+      setIsLoading(true);
+      const apiKey = process.env.REACT_APP_API_KEY;
+      try {
+        const response = await axios({
+          method: method ? method : "GET",
+          url: isTest ? testUrl : `${process.env.REACT_APP_API_URL + endpoint}`,
+          data: payload ? payload : null,
+          headers: {
+            "x-api-key": apiKey,
+          },
+        });
+        return response;
+      } catch (error) {
+        setError({
+          isError: true,
+          serverError: error,
+          status: error.response?.status,
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    []
+  );
 
   return {
     fireRequest,
