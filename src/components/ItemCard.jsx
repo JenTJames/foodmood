1523c60cbd6931cbe817e58c16d0bf0c9c9b41d9
JenTJames <jenthomasjames@gmail.com>
2023-07-09 19:12:54 +0530

    Feat: Add HealthScore to ItemCard

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,7 +1,8 @@
 import FastfoodRoundedIcon from "@mui/icons-material/FastfoodRounded";
 import AppButton from "./AppButton";
+import { Tooltip } from "@mui/material";
 
-const ItemCard = ({ image = null, title, desc }) => {
+const ItemCard = ({ image = null, title, desc = null, healthScore }) => {
   return (
     <div className="flex flex-col gap-5 p-2 rounded-lg border w-64 flex-shrink-0 lg:w-96 lg:flex-shrink justify-between">
       {!image && (
@@ -16,8 +17,15 @@ const ItemCard = ({ image = null, title, desc }) => {
           alt=""
         />
       )}
-      <h4 className="font-semibold text-2xl text-slate-800">{title}</h4>
-      <p className="text-slate-600 text-justify">{desc}</p>
+      <div className="min-w-full flex justify-between items-center gap-2">
+        <h4 className="font-semibold text-lg text-slate-800">{title}</h4>
+        <Tooltip title="Health Score">
+          <div className="w-10 h-10 flex justify-center items-center rounded-full bg-emerald-500 cursor-pointer">
+            <p className="text-slate-50">{healthScore}</p>
+          </div>
+        </Tooltip>
+      </div>
+      {desc && <p className="text-slate-600 text-justify">{desc}</p>}
       <AppButton bold variant="outlined">
         View More
       </AppButton>
